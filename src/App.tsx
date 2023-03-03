b4920316cb7278d62c8fac67b86d97bf1d178733
apetrikov <jnmort@gmail.com>
2023-03-03 13:32:01 +0300

    feat(schedule): apply fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
+import React, {ComponentProps} from 'react';
 import './App.css';
-import { Schedule } from './components'
+import {Schedule} from './components'
 
 // TODO this is only for dev, remove list import!
 import list from "./components/Schedule/tests/input";
 
 export function App() {
-  return (
-    <div className="app">
-      <Schedule
-        header="Opening hours"
-        list={list}
-      />
-    </div>
-  );
+    return (
+        <div className="app">
+            <Schedule
+              header="Opening hours"
+              list={list}
+            />
+        </div>
+    );
 }
--- src/components/Schedule/index.tsx
+++ src/components/Schedule/index.tsx
@@ -1,19 +1,38 @@
 import React from 'react';
 import styles from './Schedule.module.css'
 
-type Props = {
+export type ItemProps = {
+    item: Item
+}
+
+function Row({item}: ItemProps) {
+    const {caption, marker, list} = item
+
+    // return multiple rows
+    // return one row
+    // return one row closed
+    return (
+        <li className={styles.row}>
+            <div className={styles.caption}>{caption}</div>
+            <div className={styles.marker}>{marker}</div>
+            <div className={styles.value}>{list[0]}</div>
+        </li>
+    )
+}
+
+export type ScheduleProps = {
     header: string,
     list: Item[]
 }
 
-export function Schedule({header, list}: Props) {
+export function Schedule({header, list}: ScheduleProps) {
     return (
         <div className={styles.content}>
             <div className={styles.header}>
                 {header}
             </div>
             <ul className={styles.list}>
-                {list.map(el => <li className={styles.row}>{el.caption}</li>)}
+                {list.map(item => <Row item={item}/>)}
             </ul>
         </div>
     );
